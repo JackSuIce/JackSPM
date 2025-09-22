90a117d2ac14d5143010c05ec6b5eb4bd80034c0
Ding Tom <jack@jackdeMacBook-Pro.local>
2025-09-22 17:02:56 +0800

    no message

--- Tests/JackSPMTests/JackSPMTests.swift
+++ Tests/JackSPMTests/JackSPMTests.swift
@@ -4,13 +4,3 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
-
-import XCTest
-final class MyModuleTests: XCTestCase {
-    func testExample() {
-        // 这里编写你的测试逻辑
-        //            XCTAssertEqual(MyModule().text, "Hello, World!")
-        
-        XCTAssertEqual(MyModule().text, "Hello, World!")
-    }
-}
